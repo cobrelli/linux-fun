dc1e826eefc27fc0f183582c667dba2428d8ce18
Tomi Virtanen <cobrelli@gmail.com>
2013-11-11 11:48:32 +0200

    ..

--- Week2/bash-variables-inside-bash.sh
+++ Week2/bash-variables-inside-bash.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
-variable="test"
+variable=test
 
 echo within first shell \(pid $$\): \$variable=$variable
-bash
-echo within second shell \(pid $$\): \$variable=$variable
-exit
+bash -c 'echo within second shell \(pid $$\) \$variable=$variable'
