f6d3d75ea054872c43014b067f73c50e7f6cd472
Tomi Virtanen <cobrelli@gmail.com>
2013-11-17 23:29:37 +0200

    photoediting

--- Week3/hipstafy.sh
+++ Week3/hipstafy.sh
@@ -3,11 +3,13 @@ for file in $1/*; do
 	#echo $file
 	#inputfile=201007011200.jpg
 	inputfile=$file
-	#echo $inputfile
-	echo $file
-	prefix=${inputfile%.jpg}
-	echo $prefix
-	outputfile=$prefix-hipstah.jpg
+	echo $inputfile
+	#echo $file
+	#prefix=${inputfile%.jpg}
+	base=$(basename $inputfile)
+	prefix=${base%.jpg}
+	#echo $prefix
+	outputfile=test/$prefix-hipstah.jpg
 	echo $outputfile
 	convert -sepia-tone 60% +polaroid $inputfile $outputfile
 	#convert $inputfile -sepia-tone 60% +polaroid $outputfile
